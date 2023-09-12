3af073f8449a446d685fddcb4953e73ebfcb4fdf
Petar Arnaudov <petar.h.arnaudov@gmail.com>
2023-09-12 09:55:16 +0300

    Added attempts

--- GuessANumber.cs
+++ GuessANumber.cs
@@ -8,11 +8,19 @@ namespace GuessANumber
         {
             Random random = new Random();
             int computerNumber = random.Next(1, 101);
+            int counter = 0;
 
             while (true)
             {
+                if (counter >= 10)
+                {
+                    Console.ForegroundColor = ConsoleColor.Red;
+                    Console.WriteLine("Game over!");
+                    break;
+                }
+                Console.Write($"You have {10 - counter} more attempts left. ");
                 Console.WriteLine("Guess a number (1 - 100): ");
-
+                
                 string playerInput = Console.ReadLine();
 
                 bool isValid = int.TryParse(playerInput, out int playerNumber);
@@ -51,6 +59,7 @@ namespace GuessANumber
                 {
                     Console.WriteLine("Invalid input!");
                 }
+                counter++;
             }
         }
     }
