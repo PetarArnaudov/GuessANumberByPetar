2648c05a7bf2c4b982ac591c65236f9c89f51044
Petar Arnaudov <petar.h.arnaudov@gmail.com>
2023-09-12 09:09:48 +0300

    Create a project

---
+++ GuessANumber/Program.cs
@@ -0,0 +1,12 @@
+﻿using System;
+
+namespace GuessANumber
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Hello World!");
+        }
+    }
+}
